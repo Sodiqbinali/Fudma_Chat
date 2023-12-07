739821f7832f506206d4301b758989164e28a10e
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-07 08:43:09 +0100

    added connection to db

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const postRouter = require('./routes/PostRoute')
+
 
 //express app
 const app = express()
 
 //routes
-app.use(postRouter)
+app.use('/api/posts', postRouter)
 
-//listening for request
-app.listen(process.env.PORT, () => {
+//connect to db
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+    //listening for request
+    app.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}`)
 })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
