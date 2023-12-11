b18ae60cab45275cbefeecc014ec9e25fa2d3f82
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-11 09:48:38 +0100

    update the console message for db connection

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,9 @@ app.use('/api/posts', postRouter)
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
     //listening for request
-    app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`)
-})
+        app.listen(process.env.PORT, () => {
+        console.log(`connected to DB & listening on port ${process.env.PORT}`)
+        })
     })
     .catch((error) => {
         console.log(error)
