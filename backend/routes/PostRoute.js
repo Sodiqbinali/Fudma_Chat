48bc4ac03410fa8b74c709d0169dc8eb8e027741
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-11 09:46:41 +0100

    create post route updated

--- backend/routes/PostRoute.js
+++ backend/routes/PostRoute.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const Post = require('../models/PostModel')
 
 const router = express.Router()
 
-router.post('/',()=>{})
+router.post('/', async (req, res)=>{
+    const {userId, desc} = req.body
+
+    try {
+        const newPost = await Post.create({userId, desc})
+        res.status(200).json(newPost)
+    }   catch (error){
+        res.status(400).json({error: error.message})
+    }
+})
 router.get('/:id', ()=>{})
 router.put('/:id', ()=>{})
 router.delete('/:id', ()=>{})
