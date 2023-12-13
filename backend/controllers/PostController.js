e2355fb3c6ccc6b4a792b23084a66ba9209a1a39
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-13 08:21:56 +0100

    added create & get post controller

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -2,9 +2,35 @@ const Post = require('../models/PostModel')
 
 //Get a post
 
+const getPost = async (req, res)=>{
+    const {id} = req.params;
+
+    try {
+      const post = await Post.findById(id);
+      res.status(200).json(post);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+}
+
 //Create a post
+
+const createPost = async (req, res)=>{
+    const {userId, desc} = req.body
+
+    try {
+        const newPost = await Post.create({userId, desc})
+        res.status(200).json(newPost)
+    }   catch (error){
+        res.status(400).json({error: error.message})
+    }
+}
 
 //Update a post
 
 //delete a post
 
+module.exports = {
+    createPost,
+    getPost
+}
