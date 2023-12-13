01b49179823a8056e4edcc69a4dd85859140c840
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-13 08:23:37 +0100

    update create and get route

--- backend/routes/PostRoute.js
+++ backend/routes/PostRoute.js
@@ -1,28 +1,10 @@
 const express = require('express')
-const Post = require('../models/PostModel')
+const { createPost, getPost } = require('../controllers/PostController')
 
 const router = express.Router()
 
-router.post('/', async (req, res)=>{
-    const {userId, desc} = req.body
-
-    try {
-        const newPost = await Post.create({userId, desc})
-        res.status(200).json(newPost)
-    }   catch (error){
-        res.status(400).json({error: error.message})
-    }
-})
-router.get('/:id', async (req, res)=>{
-    const id = req.params.id;
-
-    try {
-      const post = await Post.findById(id);
-      res.status(200).json(post);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-})
+router.post('/', createPost)
+router.get('/:id', getPost)
 router.put('/:id', ()=>{})
 router.delete('/:id', ()=>{})
 router.put('/:id/like', ()=>{})
