fe29ac56b3d8b7f8905c4b35076eb8942ee17b68
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-14 14:04:59 +0100

    update post added

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -27,6 +27,20 @@ const createPost = async (req, res)=>{
 }
 
 //Update a post
+const updatePost = async (req, res) => {
+    const {id} = req.params;
+    const { userId } = req.body;
+  
+    try {
+      const post = await PostModel.findById(id);
+      if (post.userId === userId) {
+        await post.updateOne({ $set: req.body });
+        res.status(200).json("Post updated!");
+      } else {
+        res.status(403).json("Authentication failed");
+      }
+    } catch (error) {}
+  };
 
 //delete a post
 
