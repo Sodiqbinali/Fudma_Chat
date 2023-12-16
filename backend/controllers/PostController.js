3790dafc8f14c9441cdf0356146f3882cffc8d74
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-16 20:46:27 +0100

    create timeline  post control

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -60,6 +60,47 @@ export const deletePost = async (req, res) => {
   }
 };
 
+// Get timeline posts
+export const getTimelinePosts = async (req, res) => {
+  const userId = req.params.id
+  try {
+    const currentUserPosts = await PostModel.find({ userId: userId });
+
+    const followingPosts = await UserModel.aggregate([
+      { 
+        $match: {
+          _id: new mongoose.Types.ObjectId(userId),
+        },
+      },
+      {
+        $lookup: {
+          from: "posts",
+          localField: "following",
+          foreignField: "userId",
+          as: "followingPosts",
+        },
+      },
+      {
+        $project: {
+          followingPosts: 1,
+          _id: 0,
+        },
+      },
+    ]);
+
+    res.status(200).json(
+      currentUserPosts
+        .concat(...followingPosts[0].followingPosts)
+        .sort((a, b) => {
+          return new Date(b.createdAt) - new Date(a.createdAt);
+        })
+    );
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+
 module.exports = {
     createPost,
     getPost
