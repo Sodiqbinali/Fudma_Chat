103a231fa27003d48594f4c8cf91edabc600bbb2
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-18 13:56:56 +0100

    import controller and update routes

--- backend/routes/PostRoute.js
+++ backend/routes/PostRoute.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const { createPost, getPost } = require('../controllers/PostController')
+const { createPost, getPost, getTimelinePosts, deletePost, updatePost, likePost } = require('../controllers/PostController')
 
 const router = express.Router()
 
 router.post('/', createPost)
 router.get('/:id', getPost)
-router.put('/:id', ()=>{})
-router.delete('/:id', ()=>{})
-router.put('/:id/like', ()=>{})
-router.get('/:id/timeline', ()=>{})
+router.put('/:id', updatePost)
+router.delete('/:id', deletePost)
+router.put('/:id/like', likePost)
+router.get('/:id/timeline', getTimelinePosts)
 
 module.exports = router
