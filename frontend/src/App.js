20399ff42add5b8c4f09b7d30d9a1232c9354e27
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-19 08:31:44 +0100

    initial home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route  } from "react-router-dom"
+
+//Pages & component
+import home from "../pages/home";
+
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <div className="page">
+          <Routes>
+            <Route
+              path="/"
+              element={< home />}
+            />
+          </Routes>
+
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
