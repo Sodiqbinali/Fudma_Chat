9148cce90343197fcdbc0d2eb957d08ee64eb211
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-19 07:48:04 +0100

    update syntax

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -6,10 +6,10 @@ const getPost = async (req, res)=>{
     const {id} = req.params;
 
     try {
-      const post = await Post.findById(id);
-      res.status(200).json(post);
+      const post = await Post.findById(id)
+      res.status(200).json(post)
     } catch (error) {
-      res.status(400).json(error);
+      res.status(400).json(error)
     }
 }
 
@@ -28,61 +28,61 @@ const createPost = async (req, res)=>{
 
 //Update a post
 const updatePost = async (req, res) => {
-    const {id} = req.params;
-    const { userId } = req.body;
+    const {id} = req.params
+    const { userId } = req.body
   
     try {
-      const post = await Post.findById(id);
+      const post = await Post.findById(id)
       if (post.userId === userId) {
-        await post.updateOne({ $set: req.body });
-        res.status(200).json("Post updated!");
+        await post.updateOne({ $set: req.body })
+        res.status(200).json("Post updated!")
       } else {
-        res.status(403).json("Authentication failed");
+        res.status(403).json("Authentication failed")
       }
     } catch (error) {}
   };
 
 //delete a post
 const deletePost = async (req, res) => {
-  const {id} = req.params;
-  const { userId } = req.body;
+  const {id} = req.params
+  const { userId } = req.body
 
   try {
-    const post = await Post.findById(id);
+    const post = await Post.findById(id)
     if (post.userId === userId) {
-      await post.deleteOne();
-      res.status(200).json("Post deleted.");
+      await post.deleteOne()
+      res.status(200).json("Post deleted.")
     } else {
-      res.status(403).json("Action forbidden");
+      res.status(403).json("Action forbidden")
     }
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error)
   }
-};
+}
 
 // like/dislike a post
 const likePost = async (req, res) => {
-  const {id} = req.params;
-  const { userId } = req.body;
+  const {id} = req.params
+  const { userId } = req.body
   try {
-    const post = await Post.findById(id);
+    const post = await Post.findById(id)
     if (post.likes.includes(userId)) {
-      await post.updateOne({ $pull: { likes: userId } });
-      res.status(200).json("Post disliked");
+      await post.updateOne({ $pull: { likes: userId } })
+      res.status(200).json("Post disliked")
     } else {
-      await post.updateOne({ $push: { likes: userId } });
-      res.status(200).json("Post liked");
+      await post.updateOne({ $push: { likes: userId } })
+      res.status(200).json("Post liked")
     }
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error)
   }
-};
+}
 
 // Get timeline posts
 const getTimelinePosts = async (req, res) => {
   const {id} = req.params
   try {
-    const currentUserPosts = await Post.find({ userId: id });
+    const currentUserPosts = await Post.find({ userId: id })
 
     const followingPosts = await UserModel.aggregate([
       { 
@@ -110,11 +110,11 @@ const getTimelinePosts = async (req, res) => {
       currentUserPosts
         .concat(...followingPosts[0].followingPosts)
         .sort((a, b) => {
-          return new Date(b.createdAt) - new Date(a.createdAt);
+          return new Date(b.createdAt) - new Date(a.createdAt)
         })
     );
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error)
   }
 };
 
