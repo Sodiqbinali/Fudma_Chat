a0134b176f81f27a56b1b18ba819dc404e8e6dc4
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-21 07:26:14 +0100

    add home and notification to nav

--- frontend/src/components/NavIcon/NavIcon.js
+++ frontend/src/components/NavIcon/NavIcon.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './NavIcon.css'
+import Home from '../../img/home.png'
+import Noti from '../../img/noti.png'
+import {Link} from 'react-router-dom'
 
 const NavIcon = () => {
   return (
-    <div>
-      
+    <div className="navIcons">
+      <div className="home">
+         <Link to="../home">
+        <img src={Home} alt="" />
+      </Link>
+
+      {/* <UilSetting /> */}
+      <img src={Noti} alt="" />
+      </div>
     </div>
   )
 }
