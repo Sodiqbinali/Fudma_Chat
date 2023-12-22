8a9e9b7f753ace8ff23f888bccd21fe3b8d0e9d5
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-22 07:50:08 +0100

    added some static data

---
+++ frontend/src/data/TrendData.js
@@ -0,0 +1,22 @@
+export const TrendData = [
+    {
+      name: "CMP",
+      shares: 97,
+    },
+    {
+      name: "SUG",
+      shares: 80.5,
+    },
+    {
+      name: "P.Site",
+      shares: 75.5,
+    },
+    {
+      name: "ICT",
+      shares: 72,
+    },
+    {
+      name: "Sport",
+      shares: 71.9,
+    },
+  ];
