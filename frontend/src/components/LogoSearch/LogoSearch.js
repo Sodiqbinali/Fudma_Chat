c4cf6fba3dd398c7ce03c4908c0ff86b5f7fe5ed
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-27 07:32:21 +0100

    created the search bar

--- frontend/src/components/LogoSearch/LogoSearch.js
+++ frontend/src/components/LogoSearch/LogoSearch.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import './LogoSearch.css'
+import { UilSearch } from '@iconscout/react-unicons'
+import Logo from '../../img/logo.png'
 
 const LogoSearch = () => {
   return (
-    <div>
-      
+     <>
+    <div className="LogoSearch">
+      <div className="logo">
+        <img src={Logo} alt="" />
+      </div>    
+      <div className="Search">
+          <input type="text" 
+          placeholder="#Explore"
+          />
+          <div className="s-icon">
+                <UilSearch/>
+          </div>
+      </div>
     </div>
+    </>
   )
 }
 
