225980c95c8188991bfa0d58d3c6c7cd8218f931
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-30 09:57:51 +0100

    import card

--- frontend/src/components/ProfileLeft/ProfileLeft.js
+++ frontend/src/components/ProfileLeft/ProfileLeft.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import LogoSearch from '../LogoSearch/LogoSearch'
+import InfoCard from '../InfoCard/InfoCard'
 
 const ProfileLeft = () => {
   return (
     <div className='ProfileLeft'>
        <LogoSearch/>
+       <InfoCard/>
     </div>
   )
 }
