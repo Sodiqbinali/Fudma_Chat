7113de945e47bea4c24a19aad7576d992b7b0dc0
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-12-30 09:08:43 +0100

    added some static template

--- frontend/src/components/InfoCard/InfoCard.js
+++ frontend/src/components/InfoCard/InfoCard.js
@@ -1,9 +1,60 @@
 import React from 'react'
+import './infoCard.css'
+import { UilPen } from '@iconscout/react-unicons'
+import ProfileModal from "../ProfileModal/ProfileModal";
 
 const InfoCard = () => {
+
+  const [modalOpened, setModalOpened] = useState(false);
+
   return (
-    <div>
+     <div className="InfoCard">
+      <div className="infoHead">
+        <h4>Profile Info</h4>
+        
+          <div>
+            <UilPen
+              width="2rem"
+              height="1.2rem"
+              onClick={() => setModalOpened(true)}
+            />
+            <ProfileModal
+              modalOpened={modalOpened}
+              setModalOpened={setModalOpened}
+              
+            />
+          </div>
+      </div>
+
+      <div className="info">
+        {/* */}
+        <span>
+          <b>Department: </b>
+        </span>
+        <span>test</span>
+      </div>
       
+      <div className="info">
+        {/* */}
+        <span>
+          <b>Status: </b>
+        </span>
+        <span>test</span>
+      </div>
+      <div className="info">
+        <span>
+          <b>Lives in: </b>
+        </span>
+        <span>test</span>
+      </div>
+      <div className="info">
+        <span>
+          <b>Works at </b>
+        </span>
+        <span>test</span>
+      </div>
+
+      <button className="button logout-button" >Log Out</button>
     </div>
   )
 }
