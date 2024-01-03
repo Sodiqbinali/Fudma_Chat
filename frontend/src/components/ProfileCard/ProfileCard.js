129320a530a659aeefd7188a7653e096dff0545c
sodiqbinaliu <sodiboy4jamb@gmail.com>
2024-01-03 08:10:06 +0100

    create and add profile and cover pic

--- frontend/src/components/ProfileCard/ProfileCard.js
+++ frontend/src/components/ProfileCard/ProfileCard.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Cover from '../../img/defaultCover.jpg'
+import Profile from '../../img/defaultProfile.png'
+import './ProfileCard.css'
 
 const ProfileCard = () => {
   return (
-    <div>
+    <div className='ProfileCard'>
+      <div className="ProfileImages">
+        <img src={Cover} alt="" />
+        <img src={Profile} alt="" />
+      </div>
       
     </div>
   )
