5d037e6b946ea982ecbaa94af7ff753362a66d56
sodiqbinaliu <sodiboy4jamb@gmail.com>
2024-01-06 11:05:02 +0100

    create card

---
+++ frontend/src/components/FollowersCard/FollowersCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './FollowersCard.css'
+
+const FollowersCard = () => {
+  return (
+    <div className='FollowersCard'>
+        <h3>People you may know</h3>
+      
+    </div>
+  )
+}
+
+export default FollowersCard
