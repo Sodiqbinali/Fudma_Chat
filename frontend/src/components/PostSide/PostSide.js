65f959d6df8c9f465efc14accadfbe82a66fe717
sodiqbinaliu <sodiboy4jamb@gmail.com>
2024-01-15 08:35:33 +0100

    import postshare

--- frontend/src/components/PostSide/PostSide.js
+++ frontend/src/components/PostSide/PostSide.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './PostSide.css'
+import PostShare from '../PostShare/PostShare'
 
 function PostSide() {
   return (
     <div className="PostSide">
+      <PostShare />
 
     </div>
   )
